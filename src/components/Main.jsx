ecbb973faaabd8b0055864d316097308a1273221
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-02-17 00:37:10 +0100

    Add styles

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <form className="add-ingredient-form" action="">
+        <input
+          aria-label="Add ingredient"
+          placeholder="e.g. oregano"
+          type="text"
+        />
+        <button>Add ingredient</button>
+      </form>
+    </main>
+  );
+}
