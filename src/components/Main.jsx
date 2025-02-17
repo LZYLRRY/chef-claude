e9d80cbc3cc70051cd7c944b46d7e804b0e70517
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-02-17 02:28:44 +0100

    Add form & list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const listIngredients = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form" action="">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           aria-label="Add ingredient"
           placeholder="e.g. oregano"
           type="text"
+          name="ingredient"
         />
-        <button>Add ingredient</button>
+        <button type="submit">Add ingredient</button>
       </form>
+      <ul>{listIngredients}</ul>
     </main>
   );
 }
