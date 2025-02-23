64796da4c85966e16105deb3b53a647e578c095d
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-02-23 23:51:04 +0100

    Add scroll into view and useRef

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientsList from "./IngredientsList";
 
@@ -7,6 +7,8 @@ import { getRecipeFromChefClaude } from "../ai.jsx";
 export default function Main() {
   const [ingredients, setIngredients] = useState([]);
   const [recipe, setRecipe] = useState("");
+
+  const recipeSection = useRef(null);
 
   console.log(ingredients.length);
 
@@ -22,6 +24,12 @@ export default function Main() {
     setRecipe(recipeMarkdown);
   }
 
+  useEffect(() => {
+    if (recipe !== "" && recipeSection.current !== null) {
+      recipeSection.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [recipe]);
+
   return (
     <main>
       <form className="add-ingredient-form" action={addIngredient}>
@@ -34,7 +42,11 @@ export default function Main() {
         <button>Add ingredient</button>
       </form>
       {ingredients.length ? (
-        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+        <IngredientsList
+          ref={recipeSection}
+          ingredients={ingredients}
+          getRecipe={getRecipe}
+        />
       ) : null}
       {recipe ? <ClaudeRecipe recipe={recipe} /> : null}
     </main>
